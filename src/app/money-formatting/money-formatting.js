fdc34a76fde10ddbbf9845711349292ee86e7859
Danil Rezyayev <danil.rezyayev@gmail.com>
2020-05-02 18:06:44 +0300

    move money-formatting.js up

---
+++ src/app/money-formatting/money-formatting.js
@@ -0,0 +1,27 @@
+/**
+ * Formats money, inserting spaces every 3 digits and adding ruble sign
+ *
+ * @example
+ * formatMoney(42566); // '42 466₽'
+ *
+ * @param {number} money
+ * @returns {string}
+ */
+export const formatMoney = (money) => {
+	const spacedMoney = money
+		.toFixed()
+		.split('')
+		.reduceRight((formattedSalary, digit) => {
+			// length of string without spaces
+			const length = formattedSalary.split(' ').join('').length;
+
+			if (length === 0) return digit;
+
+			// insert space after every 3rd digit
+			return length % 3 === 0
+				? `${digit} ${formattedSalary}`
+				: `${digit}${formattedSalary}`;
+		}, '');
+
+	return `${spacedMoney}₽`;
+};
---
+++ src/app/money-formatting/money-formatting.test.js
@@ -0,0 +1,33 @@
+import { formatMoney } from './money-formatting.js';
+import { logTestResult } from '../modules/test-results-logging.js';
+
+const moduleName = 'MoneyFormatting';
+
+export const testMoneyFormattingModule = () => {
+	testFormatMoney();
+};
+
+const testFormatMoney = () => {
+	const logTestFormatMoneyResult = logTestResult(moduleName, formatMoney.name);
+
+	(() => {
+		const expected = '22₽';
+		const actual = formatMoney(22);
+
+		logTestFormatMoneyResult(expected, actual);
+	})();
+
+	(() => {
+		const expected = '100 000₽';
+		const actual = formatMoney(100000);
+
+		logTestFormatMoneyResult(expected, actual);
+	})();
+
+	(() => {
+		const expected = '255 255 255₽';
+		const actual = formatMoney(255255255);
+
+		logTestFormatMoneyResult(expected, actual);
+	})();
+};
