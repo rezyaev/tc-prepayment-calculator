f036b2dffe86e19c3e57f2f6afe4cac2660588fe
Danil Rezyayev <danil.rezyayev@gmail.com>
2020-05-04 13:09:02 +0300

    remove unused test.js

--- src/app/test.js
+++
@@ -1,5 +0,0 @@
-/*
-    An entry point for all test files.
-    All test functions should be imported and called here.
-    Run this file in Node.js using 'node --experimental-modules <filename>'
-*/
