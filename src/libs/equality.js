f3e700c5658b0dcd1fbe004372dfe17da09d7d96
Danil Rezyayev <danil.rezyayev@gmail.com>
2020-05-09 00:16:00 +0300

    polish, document and test equality lib

--- src/libs/equality.js
+++ src/libs/equality.js
@@ -1,67 +1,94 @@
 /** @file Module for comparing primitive values and complex objects */
 
 /**
+ * Checks if two passed arrays are equal
+ *
  * @param {any[]} firstArray
  * @param {any[]} secondArray
  * @returns {boolean}
  */
 const areArraysEqual = (firstArray, secondArray) => {
+	// empty array equals empty array
 	if (firstArray.length === 0 && secondArray.length === 0) return true;
+
+	// arrays with different lengths are not equal
 	if (firstArray.length !== secondArray.length) return false;
 
+	// get the first element from the first array
+	// and try to find the same element in the second
 	const [firstArrayElement, ...remainingFirstArray] = firstArray;
-
 	const secondArrayElement = secondArray.find((element) =>
 		areEqual(firstArrayElement, element)
 	);
 
-	if (secondArrayElement !== undefined) {
-		const secondArrayElementIndex = secondArray.indexOf(secondArrayElement);
-		const remainingSecondArray = secondArray.filter(
-			(element, index) => index !== secondArrayElementIndex
-		);
+	// if the second array hasn't the such element, they are not equal
+	if (secondArrayElement === undefined) return false;
 
-		return areArraysEqual(remainingFirstArray, remainingSecondArray);
-	}
-
-	return false;
-};
-
-/**
- * @param {object} firstObject
- * @param {object} secondObject
- * @returns {boolean}
- */
-const areObjectsEqual = (firstObject, secondObject) => {
-	const firstObjectEntries = Object.entries(firstObject);
-	const secondObjectEntries = Object.entries(secondObject);
-
-	if (firstObjectEntries.length === 0 && secondObjectEntries.length === 0)
-		return true;
-
-	if (firstObjectEntries.length !== secondObjectEntries.length) return false;
-
-	const [firstObjectEntry, ...remainingFirstEntries] = firstObjectEntries;
-
-	const secondObjectEntry = secondObjectEntries.find((entry) =>
-		areEqual(firstObjectEntry, entry)
+	// get the second array without the found element
+	const remainingSecondArray = secondArray.filter(
+		(element, index) => index !== secondArray.indexOf(secondArrayElement)
 	);
 
-	if (secondObjectEntry !== undefined) {
-		const remainingSecondEntries = secondObjectEntries.filter(
-			(entry) => entry !== secondObjectEntry
-		);
-
-		const remainingFirstObject = Object.fromEntries(remainingFirstEntries);
-		const remainingSecondObject = Object.fromEntries(remainingSecondEntries);
-
-		return areObjectsEqual(remainingFirstObject, remainingSecondObject);
-	}
-
-	return false;
+	// continue comparing the rest of the arrays
+	return areArraysEqual(remainingFirstArray, remainingSecondArray);
 };
 
 /**
+ * Checks if passed two values are equal. Complex types are compared by value,
+ * not reference.
+ *
+ * @example <caption>Comparing primitives</caption>
+ * // numbers
+ * areEqual(13, 13); // true
+ * areEqual(13, 25); // false
+ *
+ * // strings
+ * areEqual('string', 'string'); // true
+ * areEqual('string', 'not string'); // false
+ *
+ * // booleans
+ * areEqual(true, true); // true
+ * areEqual(true, false); // false
+ *
+ * @example <caption>Comparing dates</caption>
+ * const firstDate = new Date(2020, 4, 7);
+ * const firstDateClone = new Date(firstDate.getTime());
+ * const secondDate = new Date(2020, 4, 9);
+ *
+ * areEqual(firstDate, firstDateClone); // true
+ * areEqual(firstDate, secondDate); // false
+ *
+ * @example <caption>Comparing arrays</caption>
+ * // order of items is not important and items' types can be anything
+ * const array = [1, '2', 3, { a: 5 }, [true, true]];
+ * const sameArray = ['2', { a: 5 }, 3, 1, [true, true]];
+ *
+ * areEqual(array, sameArray); // true
+ * areEqual(array, [...sameArray, 4, 5]); // false
+ *
+ * @example <caption>Comparing objects</caption>
+ * // order of properties is not important and properties' types can be anything
+ * const object = {
+ *   a: 1,
+ *   bool: true,
+ *   nested: {
+ *     b: 1
+ *   },
+ *   array: [4, 5]
+ * };
+ *
+ * const sameObject = {
+ *   bool: true,
+ *   a: 1,
+ *   array: [4, 5],
+ *   nested: {
+ *     b: 1
+ *   }
+ * };
+ *
+ * areEqual(object, sameObject); // true
+ * areEqual(object, { ...sameObject, c: 15 }); // false
+ *
  * @param {any} firstValue
  * @param {any} secondValue
  * @returns {boolean}
@@ -75,8 +102,13 @@ export const areEqual = (firstValue, secondValue) => {
 	if (Array.isArray(firstValue) && Array.isArray(secondValue))
 		return areArraysEqual(firstValue, secondValue);
 
-	if (firstValue instanceof Object && secondValue instanceof Object)
-		return areObjectsEqual(firstValue, secondValue);
+	if (firstValue instanceof Object && secondValue instanceof Object) {
+		// objects are two-dimensional arrays, so compare them as arrays
+		return areArraysEqual(
+			Object.entries(firstValue),
+			Object.entries(secondValue)
+		);
+	}
 
 	return false;
 };
---
+++ src/libs/tests/equality.test.js
@@ -0,0 +1,78 @@
+import { test } from '../unit-testing.js';
+import { areEqual } from '../equality.js';
+
+const firstDate = new Date(2020, 4, 7);
+const firstDateClone = new Date(firstDate.getTime());
+const secondDate = new Date(2020, 4, 9);
+
+const array = [1, '2', 3, { a: 5 }, [true, true]];
+const sameArray = ['2', { a: 5 }, 3, 1, [true, true]];
+
+const object = {
+	a: 1,
+	bool: true,
+	nested: {
+		b: 1
+	},
+	array: [4, 5]
+};
+
+const sameObject = {
+	bool: true,
+	a: 1,
+	array: [4, 5],
+	nested: {
+		b: 1
+	}
+};
+
+test(areEqual, [
+	{
+		input: [13, 13],
+		output: true
+	},
+	{
+		input: [13, 25],
+		output: false
+	},
+	{
+		input: ['string', 'string'],
+		output: true
+	},
+	{
+		input: ['string', 'not string'],
+		output: false
+	},
+	{
+		input: [true, true],
+		output: true
+	},
+	{
+		input: [true, false],
+		output: false
+	},
+	{
+		input: [firstDate, firstDateClone],
+		output: true
+	},
+	{
+		input: [firstDate, secondDate],
+		output: false
+	},
+	{
+		input: [array, sameArray],
+		output: true
+	},
+	{
+		input: [array, [...sameArray, 4, 5]],
+		output: false
+	},
+	{
+		input: [object, sameObject],
+		output: true
+	},
+	{
+		input: [object, { ...sameObject, c: 15 }],
+		output: false
+	}
+]);
