1786eb5ac8728d98e8c60a31dda8c26838a98632
Danil Rezyayev <danil.rezyayev@gmail.com>
2020-05-09 13:07:59 +0300

    move tests to tests folder

---
+++ src/app/tests/advance-calculation.test.js
@@ -0,0 +1,21 @@
+import { test } from '../../libs/unit-testing.js';
+import { calculateAdvance } from '../advance-calculation.js';
+
+const augustIndex = 7;
+const mayIndex = 4;
+const octoberIndex = 9;
+
+test(calculateAdvance, [
+	{
+		input: [augustIndex, 50000],
+		output: 23809
+	},
+	{
+		input: [mayIndex, 142000],
+		output: 58470
+	},
+	{
+		input: [octoberIndex, 180000],
+		output: 90000
+	}
+]);
---
+++ src/app/tests/day-off.test.js
@@ -0,0 +1,26 @@
+import { isUTCDayOff } from '../day-off.js';
+import { test } from '../../libs/unit-testing.js';
+
+const saturday = new Date(Date.UTC(2020, 4, 2));
+const internationalWomenDay = new Date(Date.UTC(2020, 2, 8));
+const transferredHolidayDayOff = new Date(Date.UTC(2020, 4, 4));
+const mondayWorkday = new Date(Date.UTC(2020, 4, 18));
+
+test(isUTCDayOff, [
+	{
+		input: saturday,
+		output: true
+	},
+	{
+		input: internationalWomenDay,
+		output: true
+	},
+	{
+		input: transferredHolidayDayOff,
+		output: true
+	},
+	{
+		input: mondayWorkday,
+		output: false
+	}
+]);
